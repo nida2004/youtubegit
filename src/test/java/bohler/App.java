652048f6c23b4b1fa9ab947417f1b0e184846576
nida2004 <nyilmaz2025@gmail.com>
2020-10-13 19:52:28 +0200

    I am commit 2

--- src/test/java/bohler/App.java
+++
@@ -1,4 +0,0 @@
-package bohler;
-
-public class App {
-}
---
+++ src/test/java/bohler/Wolf.java
@@ -0,0 +1,17 @@
+package bohler;
+
+public class Wolf {
+    private static final int ID = 1;
+    private String name;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+}
