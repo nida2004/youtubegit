05a30b4b38d5f48c3a5b5ff9a14fdb03bf578991
nida2004 <nyilmaz2025@gmail.com>
2020-10-13 21:18:25 +0200

    bugfix on master

--- src/test/java/bohler/Kitten.java
+++ src/test/java/bohler/Kitten.java
@@ -1,8 +1,12 @@
 package bohler;
 
 public class Kitten {
-    public void bla(){
+    public void bla() {
         System.out.println("Blah");
         System.out.println("Blah2");
+    }
+
+    public void bugfix() {
+        System.out.println("There is a bug");
     }
 }
