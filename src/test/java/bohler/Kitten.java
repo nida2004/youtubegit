68f22aadeff0c5548224ebd759ed8e7aa355e6cb
nida2004 <nyilmaz2025@gmail.com>
2020-10-13 21:16:02 +0200

    work on feature 2.0

--- src/test/java/bohler/Kitten.java
+++ src/test/java/bohler/Kitten.java
@@ -1,8 +1,12 @@
 package bohler;
 
 public class Kitten {
-    public void bla(){
+    public void bla() {
         System.out.println("Blah");
         System.out.println("Blah2");
+    }
+
+    public void freakout() {
+        System.out.println("I am freak out ");
     }
 }
