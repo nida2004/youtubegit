dcf85d2617ce321f4129c09d9cd302779b6e5cea
nida2004 <nyilmaz2025@gmail.com>
2020-10-13 21:34:31 +0200

    fbugfix

--- src/test/java/bohler/Kitten.java
+++ src/test/java/bohler/Kitten.java
@@ -5,12 +5,17 @@ public class Kitten {
         System.out.println("Blah");
         System.out.println("Blah2");
     }
-//ven
+
+    //ven
     public void bugfix() {
         System.out.println("There is a bug");
     }
 
     public void freakout() {
         System.out.println("I am freak out ");
+    }
+
+    public void bugfix(String todo) {
+        System.out.println("Todo");
     }
 }
